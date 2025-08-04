13b9efe01b7677e9e975636c94b8efbef7a3ffe7
Tomas Barton <barton.tomas@gmail.com>
2025-08-04 16:29:28 +0200

    update raws using exiftool

--- src/geotag/geotag.py
+++ src/geotag/geotag.py
@@ -3,7 +3,7 @@ import argparse
 import os
 import json
 import pathlib
-import rdflib
+import subprocess
 import sys
 import xml.etree.ElementTree as ET
 import xml.dom.minidom
@@ -72,31 +72,99 @@ def gpx_import(args):
 
     for date, points in points_by_date.items():
         curr_date = datetime.strptime(date, "%Y-%m-%d")
-        formatted_date = curr_date.strftime("%d")
+        formatted_date = curr_date.strftime("%d.gpx")
         dir_prefix = os.path.join(
             output_dir, curr_date.strftime("%Y"), curr_date.strftime("%m")
         )
         if not os.path.isdir(dir_prefix):
             os.makedirs(dir_prefix)
-        output_file = os.path.join(dir_prefix, f"{formatted_date}.gpx")
+        output_file = os.path.join(dir_prefix, formatted_date)
         create_gpx_file(points, output_file)
         print(f"Created: {output_file}")
 
 
-def apply(args):
-    input_dir = args.input
+class GeoTagger:
+    def __init__(self, args):
+        self.dry_run = args.dry_run
+        self.verbose = args.verbose
+        self.gpx = args.gpx
+        self.input = args.input
+        self.match = args.match
 
-    if not os.path.isdir(input_dir):
-        sys.exit(f"{input_dir} was not found")
+    def date_taken(self, file):
+        """
+        Find out date when the photo was taken
+        """
+        try:
+            cmd = ["exiftool", "-DateTimeOriginal", file]
+            if self.dry_run:
+                cmd_str = " ".join(map(cmd, list))
+                print(f"DRY RUN: {cmd_str}")
+            else:
+                result = subprocess.run(cmd, capture_output=True, text=True)
+                if result.returncode != 0:
+                    print(f"erorr: {result.stderr.strip()}")
+                pos = result.stdout.find(":")
+                if pos > 0:
+                    str = result.stdout[(pos + 1) : -1].strip()
+                    # parse EXIF date
+                    taken = datetime.strptime(str, "%Y:%m:%d %H:%M:%S")
+                    return taken
+        except subprocess.TimeoutExpired:
+            if not self.dry_run:
+                print("timeout")
+        except Exception as e:
+            print(f"Error: {e}")
 
-    print(f"Processing {input_dir}")
-    print(f"searching for *.{args.match}")
-    for f in pathlib.Path(input_dir).glob(f"*.{args.match}"):
-        print(f)
-        g = Graph()
-        g.parse(f)
-        print(f"Graph g has {len(g)} statements.")
-        print(g.serialize(format="turtle"))
+    def gpx_path(self, curr_date):
+        """
+        Expand path to GPX file for given date
+        """
+        formatted_date = curr_date.strftime("%d.gpx")
+        path = os.path.join(
+            self.gpx, curr_date.strftime("%Y"), curr_date.strftime("%m"), formatted_date
+        )
+        if self.verbose:
+            print(f"expecting GPX file: {path}")
+        if not os.path.exists(path):
+            print(f"Input GPX file {path} was not found")
+        return path
+
+    def update_exif(self, photo, gpx):
+        """
+        Update photo's GPS coordinates based on GPX file
+        """
+        try:
+            cmd = ["exiftool", "-overwrite_original", "-geotag", gpx, photo]
+            if self.dry_run:
+                cmd_str = " ".join(f'"{cmd}"')
+                print(f"DRY RUN: {cmd_str}")
+            else:
+                result = subprocess.run(cmd, capture_output=True, text=True)
+                print(result.stdout)
+                if result.returncode != 0:
+                    print(f"erorr: {result.stderr.strip()}")
+        except subprocess.TimeoutExpired:
+            if not self.dry_run:
+                print("timeout")
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def apply(self):
+        input_dir = self.input
+
+        if not os.path.isdir(input_dir):
+            sys.exit(f"{input_dir} was not found")
+
+        print(f"Processing {input_dir}")
+        print(f"searching for *.{self.match}")
+        for f in pathlib.Path(input_dir).glob(f"*.{self.match}", case_sensitive=False):
+            photo_date = self.date_taken(f)
+            if photo_date:
+                gpx = self.gpx_path(photo_date)
+                if self.verbose:
+                    print(f"{f}: {photo_date} -> {gpx}")
+                self.update_exif(f, gpx)
 
 
 def cli():
@@ -115,18 +183,21 @@ def cli():
     imp.add_argument(
         "-o", "--output", type=str, help="path to GPX directory", default="gpx"
     )
-    imp.add_argument("--dry-run", help="just pretend", action="store_true")
 
     apply = subparsers.add_parser("apply", help="apply GPS data to RAW/sidecar files")
     apply.add_argument(
         "-g", "--gpx", type=str, help="path to GPX directory", default="gpx"
     )
     apply.add_argument(
-        "-m", "--match", type=str, help="file extension to match", default="xmp"
+        "-m", "--match", type=str, help="file extension to match", default="nef"
     )
     apply.add_argument(
         "-i", "--input", type=str, help="path to photos directory", required=True
     )
+    apply.add_argument(
+        "-n", "--dry-run", help="don't execute commands", action="store_true"
+    )
+    apply.add_argument("-v", "--verbose", help="verbose output", action="store_true")
 
     return parser.parse_args()
 
@@ -136,7 +207,8 @@ def main():
     if args.command == "import":
         gpx_import(args)
     elif args.command == "apply":
-        apply(args)
+        tagger = GeoTagger(args)
+        tagger.apply()
     else:
         print("unknown command, use -h/--help")
 
